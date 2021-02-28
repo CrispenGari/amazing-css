48ef1e90124805f45fdeae99688e93870ba5fcfc
CrispenGari <Crispengari@gmail.com>
2021-02-28 23:00:34 +0200

    awesome-css

---
+++ pictures.js
@@ -0,0 +1,11 @@
+const pictures = [
+  "https://picsum.photos/id/1067/640/640",
+  "https://picsum.photos/id/122/640/640",
+  "https://picsum.photos/id/188/640/640",
+  "https://picsum.photos/id/249/640/640",
+  "https://picsum.photos/id/257/640/640",
+  "https://picsum.photos/id/259/640/640",
+  "https://picsum.photos/id/283/640/640",
+  "https://picsum.photos/id/288/640/640",
+  "https://picsum.photos/id/299/640/640",
+];
